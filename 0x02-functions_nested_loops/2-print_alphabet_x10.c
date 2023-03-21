98d0b07b9a973d9b14e228a8ba1b17f7b8f738eb
Issmaile <ismail.illa205@gmail.com>
2023-03-21 16:36:42 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+ *
+ * Description: print 10 times a - z
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int line, ch;
+
+	for (line = 0; line < 10; line++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
---
+++ 0x02-functions_nested_loops/main/2-main.c
@@ -0,0 +1,17 @@
+#include "../main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: call a func that prints 10 times the alphabet
+ *
+ * Returns: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	print_alphabet_x10();
+
+	return (0);
+}
