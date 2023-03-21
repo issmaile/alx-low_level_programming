6ed7a5aacebb9c14bbdef33d155a02179e6a4caf
Issmaile <ismail.illa205@gmail.com>
2023-03-21 16:28:47 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/main/1-main.c
@@ -0,0 +1,17 @@
+#include "../main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: check the code for ALX students.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
+
