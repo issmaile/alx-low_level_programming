ad504b1bec3768fd943b14a4ab5a0b96606c840f
Issmaile <ismail.illa205@gmail.com>
2023-03-30 09:24:00 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *  *_strcat - function a
+ *
+ *  Returns: dest - destination pointer
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	while (*ptr != '\0')
+		ptr++;
+
+	while (*src != '\0')
+		*ptr++ = *src++;
+
+	*ptr = '\0';
+
+	return dest;
+}
