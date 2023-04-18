4d5bc397d5a8230948aaad7e4be7b7dc2269d163
Issmaile <ismail.illa205@gmail.com>
2023-04-17 17:56:07 -0700

    .

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
 
+typedef struct dog dog_t;
+
+
 /**
  * struct dog - a struct dog
  * @name: name
@@ -17,5 +20,6 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
 
 #endif /* DOG_H */
