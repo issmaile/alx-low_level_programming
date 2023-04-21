35dcb6c2c86e842af49fd7116be6e6b212799d42
Issmaile <ismail.illa205@gmail.com>
2023-04-21 09:22:58 -0700

    .

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+
+/**
+ * main - main function
+ * @argc: arg count
+ * @argv: arg vector
+ *
+ * Return: 98 - wrong number of args
+ * 99 - invalid operator
+ * 100 - deviding by 0
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+	char* op;
+	int b;
+	int (*operation)(int, int);
+
+	a = atoi(argv[1]);
+	op = argv[2];
+	b = atoi(argv[3]);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	operation = get_op_func(op);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", operation(a, b));
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,72 @@
+#include "3-calc.h"
+
+/**
+ * op_mod - a + b
+ * @a: arg a
+ * @b: arg b
+ *
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_mod - a - b
+ * @a: arg a
+ * @b: arg b
+ *
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a  - b);
+}
+
+/**
+ * op_mod - a * b
+ * @a: arg a
+ * @b: arg b
+ *
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_mod - a / b
+ * @a: arg a
+ * @b: arg b
+ *
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - a % b
+ * @a: arg a
+ * @b: arg b
+ *
+ * Return: a % b
+ */
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
