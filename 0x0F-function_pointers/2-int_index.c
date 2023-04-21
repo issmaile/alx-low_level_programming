dc64abf455a81958e3e40e7d30b7cc8827c9bfac
Issmaile <ismail.illa205@gmail.com>
2023-04-21 08:04:48 -0700

    .

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: array pointer
+ * @size: array size
+ * @cmp: pointer to function to be used
+ *
+ * Return: index of int inside an array
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (!array || !cmp || size <= 0)
+		return (-1);
+	
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i])
+			return (i);
+	}
+
+	return (-1);
+}
