c9d8a7fe9783aa277b7a81cd267e5c7798124eff
Issmaile <ismail.illa205@gmail.com>
2023-04-23 05:58:42 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,8 +11,12 @@ int **alloc_grid(int width, int height)
 {
 	int **gridout;
 	int i, j;
+
 	if (width < 1 || height < 1)
+	{
 		return (NULL);
+	}
+
 	gridout = malloc(height * sizeof(int *));
 	if (gridout == NULL)
 	{
