694fd5109e7b64fc759d95ebd6b2b310f9b5c0f0
Issmaile <ismail.illa205@gmail.com>
2023-04-27 06:50:05 -0700

    .

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,12 +10,10 @@ void free_list(list_t *head)
 {
 	list_t *current;
 
-	current = head;
-	while (current != NULL)
+	while ((current = head) != NULL)
 	{
 		head = head->next;
 		free(current->str);
 		free(current);
-		current = head;
 	}
 }
