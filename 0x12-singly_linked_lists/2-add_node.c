786264e50d345635ad38332ff55c9c30f18330a4
Issmaile <ismail.illa205@gmail.com>
2023-04-27 06:16:05 -0700

    .

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a node at the beginning of list_t linked list
+ * @head: list head
+ *
+ * Return: the address of the new element, or NULL if failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	char *duped_str;
+	list_t *new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = strlen(str);
+
+	new_node->next = *head;
+	*head = new_node;
+
+	return (&new_node);
+}
