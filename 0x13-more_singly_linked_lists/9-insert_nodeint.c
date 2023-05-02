b4c66f366d70a2b273ea3694fd65db18ee26c468
Issmaile <ismail.illa205@gmail.com>
2023-05-02 16:19:37 -0700

    .

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	h = *head;
 
-	h = get_nodeint(idx);
+	h = get_nodeint(idx - 1);
 
 	if (h == NULL && idx != 0)
 		return (NULL);
