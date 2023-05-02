3389261ba2098495e64d467178070d0335fb31cb
Issmaile <ismail.illa205@gmail.com>
2023-05-02 15:42:12 -0700

    .

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - free linked list
+ * @head: head
+ *
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t * tmp;
+
+	while (tmp = head)
+	{
+		head = head->next;
+		free(tmp);
+	}
+}
