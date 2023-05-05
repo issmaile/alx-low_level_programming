8367be87c8bf15ae6910f0eec8fbe67ecb047ad2
Issmaile <ismail.illa205@gmail.com>
2023-05-04 18:00:32 -0700

    .

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,13 +9,15 @@
 unsigned int binary_to_uit(const char *b)
 {
 	int len, base_two;
-	unsigned int uint = 0;
+	unsigned int uint;
 
 	if (!b)
 		return (0);
 
+	uint = 0;
 	for (len = 0; b[len] != '\0'; len++)
 		;
+
 	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
 		if (b[len] != '1' && b[len] != 0)
