10555f531a92085ae479a681e99e52c2fab281f0
issmaile <ismail25smello@gmail.com>
2023-09-09 02:21:55 +0100

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -15,7 +15,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
 			key == NULL || strlen(key) == 0)
 		return (NULL);
-	id = key_index((const unsigned char *)key, ht->index);
+	id = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[id];
 	while (tmp != NULL)
 	{
